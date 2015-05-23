8efcbe1e144145645fcea24f2a540c7cefa3fb5f
SudhaPatilI <spiosdeveloper@gmail.com>
2015-05-23 15:44:07 +0530

    Hello Commit
    Hello Log

--- ViewController.m
+++ ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"Inside Hello Git");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
